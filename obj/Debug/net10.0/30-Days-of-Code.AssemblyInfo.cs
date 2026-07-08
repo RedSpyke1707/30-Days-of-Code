807e565f99c44eb35e8ba633852c089dec0ac6fc
RedSpyke1206 <psgabriel.sp@gmail.com>
2026-07-08 18:51:35 +0000

    carpeta reto-dia-0

--- obj/Debug/net10.0/30-Days-of-Code.AssemblyInfo.cs
+++ obj/Debug/net10.0/30-Days-of-Code.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("30-Days-of-Code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdcd8cf43380e1a64c36f5115dbf42d945c5ab0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3adaa83bcd286b4bd92afa0eb0627fd473be374")]
 [assembly: System.Reflection.AssemblyProductAttribute("30-Days-of-Code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("30-Days-of-Code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
