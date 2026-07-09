2f49813a07042baa44424dc75f6cad22afd0b0c0
RedSpyke1206 <psgabriel.sp@gmail.com>
2026-07-09 01:12:11 +0000

    reto uno resuelto con javascrip

--- obj/Debug/net10.0/30-Days-of-Code.AssemblyInfo.cs
+++ obj/Debug/net10.0/30-Days-of-Code.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("30-Days-of-Code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fac9c300c64d342fa6b800303d7e5d8cf169c0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd4c7621a00b3e99837c173db40e28fd56110b42")]
 [assembly: System.Reflection.AssemblyProductAttribute("30-Days-of-Code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("30-Days-of-Code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
