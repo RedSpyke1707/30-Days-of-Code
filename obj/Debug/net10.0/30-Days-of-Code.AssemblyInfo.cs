ef1af69ec4fd003a54019ed1269f5549fb77ca68
RedSpyke1206 <psgabriel.sp@gmail.com>
2026-07-21 19:41:55 +0000

    Resultado reto dia 13 de HackerRank con Java

--- obj/Debug/net10.0/30-Days-of-Code.AssemblyInfo.cs
+++ obj/Debug/net10.0/30-Days-of-Code.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("30-Days-of-Code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db429192a954dbb52ec3f3766e58ff1ce61cc5fe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5aaae00e0449518e8ff67ae05ac3490ffa81f7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("30-Days-of-Code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("30-Days-of-Code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
