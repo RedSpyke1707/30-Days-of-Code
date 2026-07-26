60267e045e986c55f8e9dc4108eb6d68196faaf0
RedSpyke1206 <psgabriel.sp@gmail.com>
2026-07-26 23:32:49 +0000

    Reto dia 20 HackerRank

---
+++ HackerRank/Reto-dia-20/main.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    totalSwaps = 0
+    
+    for i in range(n):
+        numberOfSwaps = 0
+        
+        for j in range(n - 1):
+            if a[j] > a[j + 1]:
+                a[j], a[j + 1] = a[j + 1], a[j]
+                numberOfSwaps += 1
+                totalSwaps += 1
+                
+        if numberOfSwaps == 0:
+            break
+            
+    print(f"Array is sorted in {totalSwaps} swaps.")
+    print(f"First Element: {a[0]}")
+    print(f"Last Element: {a[-1]}")
